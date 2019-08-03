dcb5d511ed82e955caee6a76fe5c6c48d93fc45c
Peter Huyser <huyserp@gmail.com>
2019-08-03 17:02:14 +0200

    working function to create divs and append to sketchzone

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,1 +1,24 @@
-const sketchPad = document.querySelector('sketchzone');
+const sketchPad = document.querySelector('.sketchzone');
+const gridItem = document.querySelectorAll('.griditem');
+let gridDiv;
+
+function createGridDiv() {
+let sketchPadStyle = window.getComputedStyle($('html'));
+let rowNum = parseInt(sketchPadStyle.getPropertyValue('--rowNum'));
+let colNum = parseInt(sketchPadStyle.getPropertyValue('--colNum'));
+let numGridItems = rowNum * colNum;
+
+for(i=0; i<numGridItems; i++) {
+gridDiv = document.createElement('div');
+gridDiv.setAttribute('class', 'griditem');
+sketchPad.appendChild(gridDiv);
+}
+}
+/*
+function draw() {
+    need to put a loop here to assign each item to nodlist
+
+    gridItem.style.backgroundColor = 'black';
+}
+
+gridItem.addEventListener('click', draw); */
