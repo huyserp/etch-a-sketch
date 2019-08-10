03b187556d166eaefc3b2219e1d60410955585f1
Peter Huyser <huyserp@gmail.com>
2019-08-10 17:45:29 +0200

    working event listener and drawing functionality, no control buttons yet

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,24 +1,26 @@
 const sketchPad = document.querySelector('.sketchzone');
-const gridItem = document.querySelectorAll('.griditem');
+
 let gridDiv;
 
 function createGridDiv() {
-let sketchPadStyle = window.getComputedStyle($('html'));
+let sketchPadStyle = window.getComputedStyle(document.querySelector('html'));
 let rowNum = parseInt(sketchPadStyle.getPropertyValue('--rowNum'));
 let colNum = parseInt(sketchPadStyle.getPropertyValue('--colNum'));
 let numGridItems = rowNum * colNum;
 
-for(i=0; i<numGridItems; i++) {
+for(let i=0; i<numGridItems; i++) {
 gridDiv = document.createElement('div');
 gridDiv.setAttribute('class', 'griditem');
 sketchPad.appendChild(gridDiv);
 }
 }
-/*
-function draw() {
-    need to put a loop here to assign each item to nodlist
 
-    gridItem.style.backgroundColor = 'black';
+window.addEventListener('load', createGridDiv());
+
+let gridItems = sketchPad.getElementsByClassName('griditem');
+
+for (let i = 0; i < gridItems.length; i++) {
+
+    gridItems.item(i).addEventListener('mouseover', () => {gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)";});
+    
 }
-
-gridItem.addEventListener('click', draw); */
