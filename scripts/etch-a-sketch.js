cf8eb2a74de2026cbdd040c38dccc929cf65c9e2
Peter Huyser <huyserp@gmail.com>
2019-08-12 17:20:45 +0200

    can clear the grid, and set the new grid, but does not populate the sketchzone with the right ammount of divs

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,9 +1,9 @@
 const sketchPad = document.querySelector('.sketchzone');
-
+const resetBtn = document.querySelector('.resetbtn');
 let gridDiv;
 
 function createGridDiv() {
-let sketchPadStyle = window.getComputedStyle(document.querySelector('html'));
+let sketchPadStyle = window.getComputedStyle(document.querySelector('.sketchzone'));
 let rowNum = parseInt(sketchPadStyle.getPropertyValue('--rowNum'));
 let colNum = parseInt(sketchPadStyle.getPropertyValue('--colNum'));
 let numGridItems = rowNum * colNum;
@@ -20,7 +20,23 @@ window.addEventListener('load', createGridDiv());
 let gridItems = sketchPad.getElementsByClassName('griditem');
 
 for (let i = 0; i < gridItems.length; i++) {
-
-    gridItems.item(i).addEventListener('mouseover', () => {gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)";});
-    
+    gridItems.item(i).addEventListener('mouseover', 
+    () => {gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)";}); 
 }
+
+function cleanSlate() {
+    for(let i = 0; i < gridItems.length; i++) {
+        if (gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)"){
+            gridItems.item(i).style.backgroundColor = '';
+        }
+    }
+}
+
+function setGrid() {
+    let girdSize = prompt('How many squares per side would you like to make the grid?')
+    let root = document.documentElement;
+    root.style.setProperty('--rowNum', parseInt(girdSize));
+    root.style.setProperty('--colNum', parseInt(girdSize));
+}
+
+resetBtn.addEventListener('click', () => {cleanSlate(); setGrid();});
