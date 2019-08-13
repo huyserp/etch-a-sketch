64ea92f445577cb8fa96bfdf0604372de1e1a186
Peter Huyser <huyserp@gmail.com>
2019-08-13 11:24:16 +0200

    finished project working draw, change, and reset functionality

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,36 +1,5 @@
 const sketchPad = document.querySelector('.sketchzone');
 const resetBtn = document.querySelector('.resetbtn');
-let gridDiv;
-
-function createGridDiv() {
-let sketchPadStyle = window.getComputedStyle(document.querySelector('.sketchzone'));
-let rowNum = parseInt(sketchPadStyle.getPropertyValue('--rowNum'));
-let colNum = parseInt(sketchPadStyle.getPropertyValue('--colNum'));
-let numGridItems = rowNum * colNum;
-
-for(let i=0; i<numGridItems; i++) {
-gridDiv = document.createElement('div');
-gridDiv.setAttribute('class', 'griditem');
-sketchPad.appendChild(gridDiv);
-}
-}
-
-window.addEventListener('load', createGridDiv());
-
-let gridItems = sketchPad.getElementsByClassName('griditem');
-
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems.item(i).addEventListener('mouseover', 
-    () => {gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)";}); 
-}
-
-function cleanSlate() {
-    for(let i = 0; i < gridItems.length; i++) {
-        if (gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)"){
-            gridItems.item(i).style.backgroundColor = '';
-        }
-    }
-}
 
 function setGrid() {
     let girdSize = prompt('How many squares per side would you like to make the grid?')
@@ -39,4 +8,43 @@ function setGrid() {
     root.style.setProperty('--colNum', parseInt(girdSize));
 }
 
-resetBtn.addEventListener('click', () => {cleanSlate(); setGrid();});
+function createGridDiv() {
+let sketchPadStyle = window.getComputedStyle(document.querySelector('.sketchzone'));
+let rowNum = parseInt(sketchPadStyle.getPropertyValue('--rowNum'));
+let colNum = parseInt(sketchPadStyle.getPropertyValue('--colNum'));
+let numGridItems = rowNum * colNum;
+
+for(let i=0; i<numGridItems; i++) {
+let gridDiv = document.createElement('div');
+gridDiv.setAttribute('class', 'griditem');
+sketchPad.appendChild(gridDiv);
+}
+}
+
+function draw() {
+    let gridItems = document.getElementsByClassName('griditem');
+    
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems.item(i).addEventListener('mouseover', 
+        () => {gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)";}); 
+}
+}
+
+function cleanSlate() {
+    while(sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
+}
+/*
+function cleanSlate() {
+    for(let i = 0; i < gridItems.length; i++) {
+        if (gridItems.item(i).style.backgroundColor = "rgba(73, 78, 77, 0.8)"){
+            gridItems.item(i).style.backgroundColor = '';
+        }
+    }
+}
+*/
+
+window.addEventListener('load', () => {createGridDiv(); draw();});
+
+resetBtn.addEventListener('click', () => {cleanSlate(); setGrid(); createGridDiv(); draw();});
